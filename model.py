9f49e1215538a86d05f96f2595e627050148213b
Muhammed Ozmen <m.ozmenn34@gmail.com>
2023-08-01 11:26:25 +0300

    v1

---
+++ model.py
@@ -0,0 +1,220 @@
+# Import PyTorch and matplotlib
+import torch
+from torch import nn
+import matplotlib.pyplot as plt
+
+# Setup device agnostic code
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+### 1. Data
+
+# Create some data using the linear regression formula of y = weight * X + bias
+weight = 0.7
+bias = 0.3
+
+# Create range values
+start = 0
+end = 1
+step = 0.02
+
+# Create X and y (features and labels)
+X = torch.arange(start, end, step). unsqueeze(dim=1) # without unsqueeze, errors will pop up
+y = weight * X + bias
+print()
+print()
+print("X and y values:")
+print(X[:10], y[:10])
+
+# Split data
+train_split = int(0.8 * len(X))
+X_train, y_train = X[:train_split], y[:train_split]
+X_test, y_test = X[train_split:], y[train_split:]
+print()
+print()
+print("Samples must be 40 training samples and 10 testing samples.")
+print(len(X_train), len(y_train), len(X_test), len(y_test))
+
+# Plot the data
+
+# Note: if you don't have the plot_predictions() function loaded, this will give an error
+def plot_predictions(train_data=X_train, 
+                     train_labels=y_train, 
+                     test_data=X_test, 
+                     test_labels=y_test, 
+                     predictions=None):
+    """
+    Plots training data, test data and compares predictions.
+    """
+    plt.figure(figsize=(10, 7))
+
+    # Plot training data in blue
+    plt.scatter(train_data, train_labels, c="b", s=4, label="Training data")
+    
+    # Plot test data in green
+    plt.scatter(test_data, test_labels, c="g", s=4, label="Testing data")
+
+    if predictions is not None:
+        # Plot the predictions in red (predictions were made on the test data)
+        plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
+
+    # Show the legend
+    plt.legend(prop={"size": 14})
+    plt.show()
+
+plot_predictions(X_train, y_train, X_test, y_test)
+
+
+
+### 2. Building a PyTorch Linear Model
+
+# Create a linear model by subclassing nn.Module
+class LinearRegressionModelV2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Use nn.Linear() for creating the model parameters / also called as; linear transform, probing layer, fully connected layer, dense layer
+        self.linear_layer = nn.Linear(in_features=1, out_features=1)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.linear_layer(x)
+    
+# Set the manual seed
+torch.manual_seed(42)
+model_1 = LinearRegressionModelV2()
+print()
+print()
+print("Printing the model and state dict:")
+print(model_1, model_1.state_dict())
+
+# Check the model current device
+print()
+print()
+print(next(model_1.parameters()).device)  # <- Firstly its on the CPU
+
+# Set the model to use the target device
+model_1.to(device)
+print()
+print()
+print(next(model_1.parameters()).device)  # <- Now, its on the GPU
+print()
+print()
+
+
+
+### 3. Training
+
+# Setup loss function
+loss_fn = nn.L1Loss() # same as MAE
+
+# Setup our optimizer
+optimizer = torch.optim.SGD(params=model_1.parameters(), lr=0.01)
+
+# Let's write a training loop
+torch.manual_seed(42)
+
+epochs = 200
+
+# Put data on the target device (device agnostic code for data)
+X_train = X_train.to(device)
+y_train = y_train.to(device)
+X_test = X_test.to(device)
+y_test = y_test.to(device)
+
+for epoch in range(epochs):
+    model_1.train()
+
+    # 1. Forward pass
+    y_pred = model_1(X_train)
+
+    # 2. Calculate the loss
+    loss = loss_fn(y_pred, y_train)
+
+    # 3. Optimizer zero grad
+    optimizer.zero_grad()
+
+    # 4. Perform backpropagation
+    loss.backward()
+
+    # 5. Optimizer step
+    optimizer.step()
+
+    # 6. Testing
+    model_1.eval()
+    with torch.inference_mode():
+        test_pred = model_1(X_test)
+
+        test_loss = loss_fn(test_pred, y_test)
+
+    # 7. Print out what's happening
+    if epoch % 10 == 0:
+        print(f"Epoch: {epoch} | Loss: {loss} | Test loss: {test_loss}")
+
+
+### 4. Making and evaluating predictions
+
+# Turn the model into evaluation mode
+model_1.eval()
+
+# Make predictions on the test data
+with torch.inference_mode():
+    y_preds = model_1(X_test)
+
+# Check out our model predictions visually
+plot_predictions(predictions=y_preds.cpu())
+
+
+### 5. Saving & loading a trained model
+
+from pathlib import Path
+
+# 1. Create models directory
+MODEL_PATH = Path("models")
+MODEL_PATH.mkdir(parents=True, exist_ok=True)
+
+# 2. Create model save path
+MODEL_NAME = "01_pytorch_workflow_model_1.pth"
+MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
+
+# 3. Save the model state dict
+print()
+print()
+print("Model state dict:")
+print(model_1.state_dict())
+print()
+print()
+print(f"Saving model to: {MODEL_SAVE_PATH}")
+torch.save(obj=model_1.state_dict(), f=MODEL_SAVE_PATH)
+
+
+# Load a PyTorch model
+
+# Create a new instance of linear regression model V2
+loaded_model_1 = LinearRegressionModelV2()
+
+# Load the saved model_1 state_dict
+loaded_model_1.load_state_dict(torch.load(MODEL_SAVE_PATH))
+
+# Put the loaded model to device
+loaded_model_1.to(device)
+
+# Loaded model device check
+print()
+print()
+print("Loaded model device:")
+print(next(loaded_model_1.parameters()).device)
+
+# Loaded model state dict check
+print()
+print()
+print("Loaded model state dict:")
+print(loaded_model_1.state_dict())
+
+# Evaluate the loaded model
+loaded_model_1.eval()
+with torch.inference_mode():
+    loaded_model_1_preds = loaded_model_1(X_test)
+
+print()
+print()
+print("Checking the actual model and loaded model equality:")
+print(y_preds == loaded_model_1_preds)
